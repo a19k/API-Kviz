ce9c7cc1d0ff4b4d59fdb90e5e518496461e0865
Karić Anis <anekane0@gmail.com>
2025-03-16 22:24:22 +0100

    added style, wasted half dev time :)

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import getData from "./query.js";
 
 const slider = document.querySelector("#slider");
 const sliderDisplay = document.querySelector("#sliderDisplay");
+const categorySelect = document.querySelector("#categorySelect");
+const difficultySelect = document.querySelector("#difficultySelect");
+const playButton = document.querySelector(".circle");
 
 slider.max = 50;
 slider.min = 3;
@@ -12,12 +15,23 @@ slider.addEventListener("input",()=>{
     sliderDisplay.textContent = slider.value;
 })
 
+playButton.addEventListener("click",()=>{
+    console.log(categorySelect.value,difficultySelect.value);
+
+    if(categorySelect.value!=="Category"){
+        if(difficultySelect.value!=="Difficulty"){
+            localStorage.limit = slider.value;
+            localStorage.category = categorySelect.value;
+            localStorage.difficulty = difficultySelect.value;
+            window.location="./game.html"
+        }
+        else alert("Please choose a difficulty.");
+    }
+    else alert("Please choose a category.");
+});
 
 let url = "https://the-trivia-api.com/v2/questions";
-let category = "music";
-let difficulty = "easy";
-let limit = "5";
 
-url += "?limit=" + limit + "&categories=" + category + "&difficulties=" + difficulty;
+url += "?limit=" + localStorage.limit + "&categories=" + localStorage.category + "&difficulties=" + localStorage.difficulty;
 
 getData(url);
